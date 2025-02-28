6cee5df472c4884e054cc89a76f22fc649f903bc
Samuel H.Mariam <samhmariam@gmail.com>
2025-02-28 16:11:37 +0000

    update with packages

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,13 +1,14 @@
 
 WITH
 current_from_snapshot as (
-    SELECT *
-    FROM {{ ref('SNSH_ABC_BANK_POSITION') }}
-    WHERE DBT_VALID_TO is null
+    {{ current_from_snapshot(
+          snsh_ref = ref('SNSH_ABC_BANK_POSITION')
+       ) }}
 )
+
 SELECT
     *
     , POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
     , ROUND(UNREALIZED_PROFIT / COST_BASE, 5)*100
-         as UNREALIZED_PROFIT_PCT
+           as UNREALIZED_PROFIT_PCT
 FROM current_from_snapshot
--- models/staging/STG_ABC_BANK_SECURITY_INFO.sql
+++ models/staging/STG_ABC_BANK_SECURITY_INFO.sql
@@ -35,12 +35,12 @@ with_default_record as(
 
 hashed as (
     SELECT
-        concat_ws('|', SECURITY_CODE) as SECURITY_HKEY
-        , concat_ws('|', SECURITY_CODE,
-                            SECURITY_NAME, SECTOR_NAME,
-                            INDUSTRY_NAME, COUNTRY_CODE,
-                            EXCHANGE_CODE )
-                as SECURITY_HDIFF
+          {{ dbt_utils.surrogate_key([ 'SECURITY_CODE' ])
+          }} as SECURITY_HKEY
+        , {{ dbt_utils.surrogate_key([
+              'SECURITY_CODE', 'SECURITY_NAME', 'SECTOR_NAME',
+               'INDUSTRY_NAME', 'COUNTRY_CODE', 'EXCHANGE_CODE' ])
+          }} as SECURITY_HDIFF
         , * EXCLUDE LOAD_TS
         , LOAD_TS as LOAD_TS_UTC
     FROM with_default_record
